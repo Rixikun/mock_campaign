805a911653f218fa0f5b291eae30d471505e32eb
LinXia <linyianxia@hotmail.com>
2020-06-15 22:07:32 -0400

    responsive home, use grids

--- src/components/content/Home.js
+++ src/components/content/Home.js
@@ -1,7 +1,92 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <div className="home__section home__landing">
+        <div className="landing__info">
+          Meet Candidate ___ !
+          <br />
+          Representing X , Y , Z
+          <br />
+          Join now!
+        </div>
+        <button>Volunteer here</button>
+      </div>
+      <div className="grid__container">
+        <div className="home__section section1">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget
+            dui turpis. Nunc ante arcu, euismod eu condimentum condimentum,
+            eleifend in dui. Proin in ante non ipsum molestie efficitur. Donec
+            ut felis vestibulum, volutpat lorem id, ultrices ante. Proin eros
+            orci, hendrerit quis urna ut, maximus luctus nunc. Cras ut velit
+            risus. Ut tincidunt vel enim sed posuere. Nunc venenatis ut nisi sed
+            fermentum. Quisque non aliquam sem, id tristique sem. Suspendisse at
+            hendrerit ante. Ut ipsum sem, fringilla sed elementum ac, egestas
+            vel lorem. Sed tempor scelerisque lacus. Mauris pretium a est et
+            ornare.
+          </p>
+        </div>
+        <div className="home__section section2">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget
+            dui turpis. Nunc ante arcu, euismod eu condimentum condimentum,
+            eleifend in dui. Proin in ante non ipsum molestie efficitur. Donec
+            ut felis vestibulum, volutpat lorem id, ultrices ante. Proin eros
+            orci, hendrerit quis urna ut, maximus luctus nunc. Cras ut velit
+            risus. Ut tincidunt vel enim sed posuere. Nunc venenatis ut nisi sed
+            fermentum. Quisque non aliquam sem, id tristique sem. Suspendisse at
+            hendrerit ante. Ut ipsum sem, fringilla sed elementum ac, egestas
+            vel lorem. Sed tempor scelerisque lacus. Mauris pretium a est et
+            ornare.
+          </p>
+        </div>
+        <div className="home__section section3">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget
+            dui turpis. Nunc ante arcu, euismod eu condimentum condimentum,
+            eleifend in dui. Proin in ante non ipsum molestie efficitur. Donec
+            ut felis vestibulum, volutpat lorem id, ultrices ante. Proin eros
+            orci, hendrerit quis urna ut, maximus luctus nunc. Cras ut velit
+            risus. Ut tincidunt vel enim sed posuere. Nunc venenatis ut nisi sed
+            fermentum. Quisque non aliquam sem, id tristique sem. Suspendisse at
+            hendrerit ante. Ut ipsum sem, fringilla sed elementum ac, egestas
+            vel lorem. Sed tempor scelerisque lacus. Mauris pretium a est et
+            ornare.
+          </p>
+        </div>
+        <div className="home__section section4">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget
+            dui turpis. Nunc ante arcu, euismod eu condimentum condimentum,
+            eleifend in dui. Proin in ante non ipsum molestie efficitur. Donec
+            ut felis vestibulum, volutpat lorem id, ultrices ante. Proin eros
+            orci, hendrerit quis urna ut, maximus luctus nunc. Cras ut velit
+            risus. Ut tincidunt vel enim sed posuere. Nunc venenatis ut nisi sed
+            fermentum. Quisque non aliquam sem, id tristique sem. Suspendisse at
+            hendrerit ante. Ut ipsum sem, fringilla sed elementum ac, egestas
+            vel lorem. Sed tempor scelerisque lacus. Mauris pretium a est et
+            ornare.
+          </p>
+        </div>
+      </div>
+
+      <div className="home__section section5">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget dui
+          turpis. Nunc ante arcu, euismod eu condimentum condimentum, eleifend
+          in dui. Proin in ante non ipsum molestie efficitur. Donec ut felis
+          vestibulum, volutpat lorem id, ultrices ante. Proin eros orci,
+          hendrerit quis urna ut, maximus luctus nunc. Cras ut velit risus. Ut
+          tincidunt vel enim sed posuere. Nunc venenatis ut nisi sed fermentum.
+          Quisque non aliquam sem, id tristique sem. Suspendisse at hendrerit
+          ante. Ut ipsum sem, fringilla sed elementum ac, egestas vel lorem. Sed
+          tempor scelerisque lacus. Mauris pretium a est et ornare.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
