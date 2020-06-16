d6cc5128766d5723f061b62713365d732a375209
LinXia <linyianxia@hotmail.com>
2020-06-16 01:59:32 -0400

    wip: truncating individual elements

--- src/components/content/Endorsement.js
+++ src/components/content/Endorsement.js
@@ -26,26 +26,54 @@ const Endorsement = ({ peopleData, fetchPeople }) => {
         <h3>Endorsement</h3>
       </div>
       <div className="endorsement__container">
-        {peopleData &&
-          peopleData.users &&
-          peopleData.users.map((person) => (
-            <div className="person">
-              <div className="photo__container">
-                <div className="photo photo2">{person.name}</div>
-              </div>
-              <div className={`description ${truncate ? "expanded" : ""}`}>
-                <p key={person.id} onClick={(e) => toggle(e)}>
-                  {person.name} of {person.address.city}
-                  stands in support of this candidate!
-                  <br />
-                  {Number(person.id) % 2
-                    ? `Fusce ligula mi, congue ut cursus nec, dapibus a arcu. Suspendisse sit amet leo arcu. Etiam vel ligula ut augue molestie sodales. Integer gravida sed lorem at efficitur. Maecenas non nulla eget orci convallis vulputate vitae non ex. Donec quis libero ut nulla posuere elementum nec a quam. Pellentesque arcu mi, maximus ac metus vitae, convallis porta nisl. Morbi laoreet, nisl at laoreet imperdiet, eros est egestas felis, sit amet consequat eros urna ullamcorper mauris. `
-                    : `Donec suscipit nisi a dolor finibus mattis eget a elit. Nunc vel posuere lacus. Maecenas imperdiet sapien at pulvinar egestas. Vivamus lacinia orci sed orci sollicitudin, non vestibulum justo condimentum. Nam nec quam ultrices, sollicitudin tellus in, facilisis magna. Vivamus molestie posuere nisi, sed convallis dui fermentum sed. Phasellus ex elit, consequat at nunc et, pulvinar volutpat enim. Praesent efficitur felis id lobortis feugiat. Integer arcu velit, hendrerit tempus leo a, consequat efficitur lacus. Donec auctor quam non justo egestas, in commodo nulla tincidunt. Mauris nec est porttitor, viverra erat ac, varius nulla. Donec justo justo, feugiat non augue a, laoreet venenatis metus. Sed finibus velit felis, nec auctor neque maximus nec. Donec leo nunc, mattis a neque ac, auctor lacinia lacus. Cras nec augue et metus egestas lacinia. Donec tempor non odio at tempus.`}
-                </p>
-              </div>
-            </div>
-          ))}
+        <div className="person">
+          <div className="photo__container">
+            <div className="photo photo2">dummy</div>
+          </div>
+          <div className={`description ${truncate ? "expanded" : ""}`}>
+            <span
+              onClick={(e) => toggle(e)}
+              className={`${
+                truncate ? "fa fa-chevron-up" : "fa fa-chevron-down"
+              }`}
+            ></span>
+
+            <p key="dummy" onClick={(e) => toggle(e)}>
+              dummy person of some town stands in support of this candidate!
+              <br />
+              this is osme short bio text
+            </p>
+          </div>
+        </div>
       </div>
+
+      {peopleData &&
+        peopleData.users &&
+        peopleData.users.map((person) => (
+          <div className="person">
+            <div className="photo__container">
+              <div className="photo photo2">{person.name}</div>
+            </div>
+            <div className={`description ${truncate ? "expanded" : ""}`}>
+              <span
+                onClick={(e) => toggle(e)}
+                className={`${
+                  truncate ? "fa fa-chevron-up" : "fa fa-chevron-down"
+                }`}
+              ></span>
+
+              <p key={person.id} onClick={(e) => toggle(e)}>
+                {person.name} of {person.address.city}
+                stands in support of this candidate!
+                <br />
+                {Number(person.id) % 2
+                  ? `Fusce ligula mi, congue ut cursus nec, dapibus a arcu. Suspendisse sit amet leo arcu. Etiam vel ligula ut augue molestie sodales. Integer gravida sed lorem at efficitur. Maecenas non nulla eget orci convallis vulputate vitae non ex. Donec quis libero ut nulla posuere elementum nec a quam. Pellentesque arcu mi, maximus ac metus vitae, convallis porta nisl. Morbi laoreet, nisl at laoreet imperdiet, eros est egestas felis, sit amet consequat eros urna ullamcorper mauris. `
+                  : `Donec suscipit nisi a dolor finibus mattis eget a elit. Nunc vel posuere lacus. Maecenas imperdiet sapien at pulvinar egestas. Vivamus lacinia orci sed orci sollicitudin, non vestibulum justo condimentum. Nam nec quam ultrices, sollicitudin tellus in, facilisis magna. Vivamus molestie posuere nisi, sed convallis dui fermentum sed. Phasellus ex elit, consequat at nunc et, pulvinar volutpat enim. Praesent efficitur felis id lobortis feugiat. Integer arcu velit, hendrerit tempus leo a, consequat efficitur lacus. Donec auctor quam non justo egestas, in commodo nulla tincidunt. Mauris nec est porttitor, viverra erat ac, varius nulla. Donec justo justo, feugiat non augue a, laoreet venenatis metus. Sed finibus velit felis, nec auctor neque maximus nec. Donec leo nunc, mattis a neque ac, auctor lacinia lacus. Cras nec augue et metus egestas lacinia. Donec tempor non odio at tempus.`}
+              </p>
+            </div>
+          </div>
+        ))}
+
       <div className="publicity__container">
         {publicity.map((logo) => (
           <div className="company">
