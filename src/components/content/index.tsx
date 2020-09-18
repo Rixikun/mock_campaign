5a61a78bb121aebadc04be7ee97072823585ef28
LinXia <linyianxia@hotmail.com>
2020-09-17 23:16:26 -0400

    fix /component/content/index

---
+++ src/components/content/index.tsx
@@ -0,0 +1,29 @@
+import AboutCandidate from "./AboutCandidate";
+import Donate from "./Donate";
+import Endorsement from "./Endorsement";
+import Footer from "./Footer";
+import Header from "./Header";
+import Home from "./Home";
+import HowTo from "./HowTo";
+import Merchandise from "./Merchandise";
+import Navigation from "./Navigation";
+import SingleUser from "./SingleUser";
+import Stances from "./Stances";
+import Volunteer from "./Volunteer";
+import VolunteerForm from "./VolunteerForm";
+
+export {
+  AboutCandidate,
+  Donate,
+  Endorsement,
+  Footer,
+  Header,
+  Home,
+  HowTo,
+  Merchandise,
+  Navigation,
+  SingleUser,
+  Stances,
+  Volunteer,
+  VolunteerForm,
+};
